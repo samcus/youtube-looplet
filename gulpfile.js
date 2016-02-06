2523952385466d8ea2df9903a03287da127d5303
samcus <samuel.a.custer@gmail.com>
2016-02-05 22:23:09 -0500

    Add gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ var
 gulp = require("gulp"),
 sass = require("gulp-sass"),
 sassGlob = require("gulp-sass-glob");
+watch = require("gulp-watch")
 
 gulp.task('default', ['sass']);
 
-gulp.task('sass', function () {
+gulp.task('sass', function(){
   gulp.src('src/scss/**/*.scss')
     .pipe(sassGlob())
     .pipe(sass({}).on('error', sass.logError))
     .pipe(gulp.dest('resources/stylesheets'));
 });
+
+gulp.watch('src/scss/**/*.scss', ['sass']);
