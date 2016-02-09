d0cc9f71687163c9ccf4d34fc619330cd35ca6c3
samcus <samuel.a.custer@gmail.com>
2016-02-08 19:21:49 -0500

    Add javascript concat to workflow, add sniff.js library

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 var
 gulp = require("gulp"),
 sass = require("gulp-sass"),
-sassGlob = require("gulp-sass-glob");
-watch = require("gulp-watch")
+sassGlob = require("gulp-sass-glob"),
+watch = require("gulp-watch"),
+concat = require("gulp-concat");
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'concatJS']);
 
 gulp.task('sass', function(){
   gulp.src('src/scss/**/*.scss')
     .pipe(sassGlob())
     .pipe(sass({}).on('error', sass.logError))
     .pipe(gulp.dest('resources/stylesheets'));
+});
+
+gulp.task('concatJS', function(){
+  return gulp.src('src/javascript/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('resources/javascript'));
 });
 
 gulp.watch('src/scss/**/*.scss', ['sass']);
