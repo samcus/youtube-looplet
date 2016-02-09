7d6e8a41e7ee88e3ff18d3ae39350b361a1f016b
samcus <samuel.a.custer@gmail.com>
2016-02-08 19:29:57 -0500

    Replace gulp-change with gulp-changed, add concatJS watcher

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ gulp = require("gulp"),
 sass = require("gulp-sass"),
 sassGlob = require("gulp-sass-glob"),
 watch = require("gulp-watch"),
-concat = require("gulp-concat");
+concat = require("gulp-concat"),
+changed = require('gulp-changed');
 
 gulp.task('default', ['sass', 'concatJS']);
 
@@ -16,8 +17,10 @@ gulp.task('sass', function(){
 
 gulp.task('concatJS', function(){
   return gulp.src('src/javascript/**/*.js')
+    .pipe(changed('resources/javascript'))
     .pipe(concat('main.js'))
     .pipe(gulp.dest('resources/javascript'));
 });
 
 gulp.watch('src/scss/**/*.scss', ['sass']);
+gulp.watch('src/javascript/**/*.js', ['concatJS']);
