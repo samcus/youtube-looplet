c17ff5271fa55a1fb4317e3462316cf1a0b40b5b
samcus <samuel.a.custer@gmail.com>
2016-06-05 04:45:36 -0400

    Added Heroku

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express"),
+favicon = require('express-favicon'),
+app = express();
+
+NODE_ENV="production";
+
+app.use(favicon(__dirname + '/favicon.png'));
+
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (req, res, next) {
+  res.render('home.ejs',{});
+  next();
+});
+
+app.get('*',function(req,res,next){
+  if(!res.headerSent){
+    if(res.status(404)){
+      res.render('404.ejs',{
+        location: "404"
+      });
+    }
+  }
+});
+
+
+
+app.listen(process.env.PORT || 8003, function () {
+  console.log('Example app listening on port 80!', process.env.PORT );
+});
